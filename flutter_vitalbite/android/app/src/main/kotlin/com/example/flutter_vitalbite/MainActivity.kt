f04f8d99a454be0fcab605b0d9c8abab789047ca
Carmen Fernández de Córdoba <carmenfdcpwork@gmail.com>
2026-02-16 11:56:15 +0100

    Proyecto flutter creado

---
+++ flutter_vitalbite/android/app/src/main/kotlin/com/example/flutter_vitalbite/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_vitalbite
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ flutter_vitalbite/lib/features/login/ui/login_page_view.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class LoginPageView extends StatelessWidget {
+  const LoginPageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+              image: const DecorationImage(
+                image: AssetImage('assets/fondoVitalBite.png'),
+                fit: BoxFit.cover,
+              ),
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.transparent, Colors.black.withOpacity(0.6)],
+              ),
+            ),
+          ),
+          SafeArea(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text('Welcome to', style: TextStyle(fontSize: 48)),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
